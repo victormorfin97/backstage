942d84f70f1237a9e49a676b976cc78c26382ee3
Fredrik Adelöw <freben@gmail.com>
2021-03-30 23:04:05 +0200

    Use a simpler catalog filter context
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

---
+++ plugins/catalog/src/filter2/internal/context.tsx
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Entity } from '@backstage/catalog-model';
+import { createContext } from 'react';
+import { Filter } from '../types';
+
+export type FiltersContext = {
+  loading: boolean;
+  error: Error | undefined;
+  serverEntities: Entity[];
+  entities: Entity[];
+  registerFilter: (initialValue?: () => Filter) => string;
+  unregisterFilter: (id: string) => void;
+  setFilter: (id: string, filter: Filter) => void;
+};
+
+export const filtersContext = createContext<FiltersContext | undefined>(
+  undefined,
+);
